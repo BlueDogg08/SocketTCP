4a039ee2f238a24a83ad4f88121e4ea5b9abf6d3
BlueDogg08 <94381985+BlueDogg08@users.noreply.github.com>
2022-04-21 19:03:54 +0200

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -13,6 +13,16 @@ public class Client {
       Socket socket= new Socket(ip, port);
       /*in= new BufferedReader(new InputStreamReader(socket.getInputStream()));
       out= new PrintStream(socket.getOutputStream(), true);*/
+      
+      //Ricezione dal Server 2° passaggio
+      DataInputStream dis=new DataInputStream(socket.getInputStream());
+      String str=(String)dis.readUTF();
+      
+      //Invio al Server 3° passaggio
+      DataOutputStream dout=new DataOutputStream(socket.getOutputStream());
+      dout.writeUTF(str+"\r\nSocket: Ok");
+      dout.flush();
+      dout.close();
       socket.close();
     }catch(IOException ioe){
         System.err.println("Eccezione IO");

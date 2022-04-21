ad243b462d08ad1c09d6eb4ad33f2dec358b1fe2
BlueDogg08 <94381985+BlueDogg08@users.noreply.github.com>
2022-04-21 19:04:12 +0200

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -6,18 +6,28 @@ public class Server {
       
     int port=2000;
     Socket socket= null;
-      
+    
     try{
       //apertura porta
       //server in ascolto
       ServerSocket sSocket= new ServerSocket(port);
       System.out.println("In attesa di connetersi con il client");
-            
+      
       socket=sSocket.accept();
       System.out.println("Server connesso con il socket: "+ socket.getRemoteSocketAddress());
-            
-      //scambio dati
-      socket.getInputStream();
+      
+      //Invio al client 1° passaggio
+      DataOutputStream dout=new DataOutputStream(socket.getOutputStream());
+      dout.writeUTF("Server: Connessione avvenuta");
+      dout.flush();
+      dout.close();
+      
+      //Ricezione dal client  4° passaggio
+      DataInputStream dis=new DataInputStream(socket.getInputStream());
+      String str=(String)dis.readUTF();
+      System.out.println(str);
+      sSocket.close();
+      
     }catch(BindException be){
       System.err.println("Server già avviato");
     }catch(Exception e){
